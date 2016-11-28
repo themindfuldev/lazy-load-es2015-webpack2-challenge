92a934d1ef46579d406b24d8d92f4c0d4c5c44f8
Tiago Garcia <tiago.garcia@macys.com>
2016-11-27 20:26:38 -0800

    fixing comments

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-// Importing Cat module synchronously
+// Importing from Animations and Messaging
 import { MerryXmas } from './animations';
 import { Alert } from './messaging'
 
-// Listener to lazy load Zoo
+// Listener to fire up Merry Xmas
 document.getElementById( 'button' ).addEventListener( 'click', e => {
   if (!this.merryXmas) {
     this.merryXmas = new MerryXmas( 'effect' );
@@ -10,6 +10,7 @@ document.getElementById( 'button' ).addEventListener( 'click', e => {
   this.merryXmas.animate();
 } );
 
+// Timeout to fire up Alert
 setTimeout(() => {
   new Alert();
 }, 5000);
